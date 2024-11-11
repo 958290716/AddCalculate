b6989e9cc84dd31c54a35f1d6ae3941f064911dd
karsonjo <mkr67n@outlook.com>
2024-11-11 15:09:48 +0800

    Fix NaiveCalculator.

--- Caculate1/Calculator/NaiveCalculator.cs
+++ Caculate1/Calculator/NaiveCalculator.cs
@@ -2,94 +2,27 @@
 
 namespace ConsoleCalculator.Calculator;
 
-public class NaiveCalculator : ICalculator
+public class NaiveCalculator(ITokenParser _parser) : ICalculator
 {
-    private readonly ITokenParser _parser;
-
-    public NaiveCalculator(ITokenParser tokenParser)
-    {
-        _parser = tokenParser;
-    }
-
     public double Calculate(string expression)
     {
-        double result = 0;
-        string tempOperator1 = "";
-        string tempOperator2 = "";
-        //int seq = 0; //record list position
-        int isNum = 0; // record is a Number or not
-        int isOperator = 0; //record is a Operator or not
-        List<Token> listToken1 = _parser.Parse(expression);
-        foreach(Token T1 in listToken1)
+        List<Token> tokens = _parser.Parse(expression);
+
+        if (tokens.Count != 3)
+            throw new Exception("Invalid expression");
+        if (tokens[0].Type != TokenType.Number || tokens[2].Type != TokenType.Number)
+            throw new Exception("Invalid expression");
+        if (tokens[1].Type != TokenType.Operator)
+            throw new Exception("Invalid expression");
+
+        var num1 = double.Parse(tokens[0].Value);
+        var num2 = double.Parse(tokens[2].Value);
+
+        return tokens[1].Value switch
         {
-            if(isNum == 0 && isOperator == 0) //both isNum and isOperator is 0,means start of the Tokenlist
-            {
-                if(T1.Type == TokenType.Number)
-                {
-                    result = int.Parse(T1.Value);                   
-                }
-                if(T1.Type == TokenType.Operator)
-                {
-                    isOperator = 1;
-                    tempOperator1 = T1.Value;
-                    continue;
-                }
-            }
-            
-            if(isOperator == 1) //isOperator is 1 means just add or minus
-            {
-                if (T1.Type == TokenType.Number) //just one operator, just add or minus
-                {
-                    if (tempOperator1 == "+")
-                    {
-                        result += int.Parse(T1.Value);
-                        isOperator = 0; // reset isOperator
-                    }
-                    else if (tempOperator1 == "-")
-                    {
-                        result += (-1) * int.Parse(T1.Value);
-                        isOperator = 0; // reset isOperator
-                    }
-                }
-                else if (T1.Type == TokenType.Operator)  
-                {
-                    tempOperator2 = T1.Value;
-                    isOperator = 2;
-                    continue;
-                }
-            }
-            if (isOperator == 2) //isOperator is 2 means need process the number first,only number after two operators
-            { 
-                if(tempOperator2 == "+" && tempOperator1 == "+") 
-                {
-                    result += int.Parse(T1.Value);
-                    tempOperator1 = "";
-                    tempOperator2 = "";
-                    isOperator = 0;
-                }
-                else if(tempOperator2 == "-" && tempOperator1 == "+")
-                {
-                    result += (-1) * int.Parse(T1.Value);
-                    tempOperator1 = "";
-                    tempOperator2 = "";
-                    isOperator = 0;
-                }
-                else if (tempOperator2 == "+" && tempOperator1 == "-")
-                {
-                    result -= int.Parse(T1.Value);
-                    tempOperator1 = "";
-                    tempOperator2 = "";
-                    isOperator = 0;
-                }
-                else if (tempOperator2 == "-" && tempOperator1 == "-")
-                {
-                    result -= (-1) * int.Parse(T1.Value);
-                    tempOperator1 = "";
-                    tempOperator2 = "";
-                    isOperator = 0;
-                }
-            }
-        }
-        return result;
+            "+" => num1 + num2,
+            "-" => num1 - num2,
+            _ => throw new Exception("Unsupported operation"),
+        };
     }
 }
